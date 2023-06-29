51f29c45bb78423f5a3f92200a335f75163b0a81
rhkk7157 <rhkk7157@gmail.com>
2023-06-30 04:13:08 +0900

    gnb 추가

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import { QueryClientProvider } from "react-query"
 import { getClient } from "./queryClient"
 import { ReactQueryDevtools } from 'react-query/devtools'
+import Gnb from './components/gnb'
 
 const App = () => {
   const queryClient = getClient()
   return (
     <QueryClientProvider client={queryClient}>
+      <Gnb />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
