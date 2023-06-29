6b9bbb8efa4d5f9ab2a9a47929ca692a0ccc350b
rhkk7157 <rhkk7157@gmail.com>
2023-06-30 03:25:10 +0900

    queryClient option 설정

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -7,13 +7,23 @@ import {
 } from 'react-query'
 // import { getTodos, postTodo } from '../my-api'
 
-type AnyOBJ = { [key: string]: any }
+type AnyOBJ = { [key: string]: unknown }
 
 // Create a client
 export const getClient = (() => {
   let client: QueryClient | null = null;
   return () => {
-    if(!client) client = new QueryClient({})
+    if(!client) client = new QueryClient({
+      defaultOptions: {
+        queries: {
+          cacheTime: 1000 * 60 * 60 * 24,
+          staleTime: 1000 * 60,
+          refetchOnMount: false,
+          refetchOnReconnect: false,
+          refetchOnWindowFocus: false,
+        },
+      }
+    })
     return client
   }
 })()
@@ -32,7 +42,7 @@ export const fetcher = async ({
   params?:AnyOBJ;
 }) => {
   try {
-    const url = `${BASE_URL}${path}`
+    let url = `${BASE_URL}${path}`
     const fetchOptions: RequestInit = {
       method,
       headers: {
@@ -40,6 +50,14 @@ export const fetcher = async ({
         'Access-Control-Allow-Origin': BASE_URL
       }
     }
+
+    if(params) {
+      const searchParams = new URLSearchParams(params)
+      url += '?'+ searchParams.toString()
+    }
+
+    if(body) fetchOptions.body = JSON.stringify(body)
+    
     const res = await fetch(url, fetchOptions)
     const json = await res.json()
     return json
