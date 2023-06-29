35c6883f08fd8e7fa5ff032cb10eef791e1d211f
rhkk7157 <rhkk7157@gmail.com>
2023-06-30 03:32:41 +0900

    상품 상세 페이지 추가

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,5 +1,25 @@
-const ProductDetail = () => (
-  <div>상세 페이지</div>
-)
+import { useQuery } from "react-query"
+import { Product } from "../../types"
+import { QueryKeys, fetcher } from "../../queryClient"
+import { useParams } from "react-router-dom"
+import ProductDetail from "../../components/product/detail"
 
-export default ProductDetail
+const ProductDetailPage = () => {
+  const { id } = useParams()
+
+  const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () => fetcher({
+    method: 'GET',
+    path: `/products/${id}`
+  })) 
+
+  if(!data) return null
+
+  return (
+    <>
+      <h2>상품 상세</h2>
+      <ProductDetail item={data} />
+    </>
+  )
+}
+
+export default ProductDetailPage
